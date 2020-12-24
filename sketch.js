b4f6659366f6d778fd4f58f5920c198c4e531b56
Pratyush615 <75452860+Pratyush615@users.noreply.github.com>
2020-12-24 11:28:50 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,22 +1,33 @@
-var fixedRect, movingRect;
+var car, wall;
+var speed, width;
 
 function setup() {
-  createCanvas(1200,800);
-  fixedRect = createSprite(400, 100, 50, 80);
-  fixedRect.shapeColor = "green";
-  fixedRect.debug = true;
-  movingRect = createSprite(400, 800,80,30);
-  movingRect.shapeColor = "green";
-  movingRect.debug = true;
-
-  movingRect.velocityY = -5;
-  fixedRect.velocityY = +5;
+  createCanvas(1600,400);
+  car=createSprite(50,200,50,50);
+  speed=random(55,90);
+  weight=random(400,1500);
+  wall=createSprite(1000,200,60,height/2);
+  car.velocityX=speed;
 }
 
 function draw() {
-  background(0,0,0);  
+  background(255,255,255);  
+  if (wall.x - car.x < (car.width+wall.width)/2) {
+    car.velocityX=0;
+    var deformation=0.5*weight*speed*speed/22500;
+    if (deformation>180){
+  
+    car.shapeColor=color(255,0,0);
+  }
+  if(deformation<180 && deformation>100){
+  
+    car.shapeColor=color(230,230,0);
+  }
+  if(deformation<100) {
+    car.shapeColor=color(0,255,0);
+  }
+ 
+}
+drawSprites();
 
- bounceOff(movingRect, fixedRect);
-
-  drawSprites();
 }
